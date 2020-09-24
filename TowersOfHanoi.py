c9eaad72032d7675aee0becebcade2ab82c5bea4
Patrick May <patrickpm.may@gmail.com>
2020-09-24 12:46:58 -0500

    InitialCommit

---
+++ TowersOfHanoi.py
@@ -0,0 +1,19 @@
+"""
+Program to recursively solve the towers of hanoi of any size
+"""
+
+
+def print_move(fr, to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        print_move(fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+
+
+print(Towers(4, 'P1', 'P2', 'P3'))
